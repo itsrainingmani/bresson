22d0989445efa4bf6b0a53d5397dceea500bc5c6
Mani Sundararajan <10191300+itsrainingmani@users.noreply.github.com>
2024-01-10 17:46:07 -0500

    add anyhow and exif parsing lib

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
-fn main() -> std::io::Result<()> {
-    let image_file = std::path::Path::new("./test.jpg");
+use anyhow::Result;
+use bresson::*;
+use std::path::Path;
+
+fn main() -> Result<()> {
+    let image_arg = std::env::args().nth(1).unwrap();
+    let image_file = Path::new(&image_arg);
     if image_file.is_file() {
         println!("Image: {}", image_file.display());
     } else {
         println!("Image not present");
     }
+
+    read_metadata(image_file)?;
 
     Ok(())
 }
