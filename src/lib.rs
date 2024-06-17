3b608de92f5bb7c999959e2f4ad55c357aaa2016
Mani Sundararajan <10191300+itsrainingmani@users.noreply.github.com>
2024-06-17 18:16:27 -0400

    move image code to sep module

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
 pub mod globe;
-=======
+pub mod image;
 pub mod randomize;
->>>>>>> d1dd138 (abstract randomization. fix some bugs related to removing image render)
 pub mod state;
 pub mod ui;
 pub mod utils;
