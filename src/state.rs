bfe34dd71a6986fa807ccf15779154f9f7a4bf35
Mani Sundararajan <10191300+itsrainingmani@users.noreply.github.com>
2024-06-19 15:16:43 -0400

    trying some stuff around formatting each cell as multi-lines

--- src/state.rs
+++ src/state.rs
@@ -2,12 +2,19 @@ use anyhow::Result;
 use core::f32;
 use exif::{experimental::Writer, Exif, Field, In, Rational, SRational, Tag, Value};
 use globe::Globe;
-use ratatui::{layout::Rect, widgets::Row};
+use ratatui::{
+    layout::Rect,
+    style::{Style, Stylize},
+    text::{Line, Text},
+    widgets::{Cell, Paragraph, Row, Wrap},
+};
 use ratatui_image::{protocol::StatefulProtocol, Resize};
 use std::{
     collections::HashMap,
     io::{self, Read, Write},
     path::{Path, PathBuf},
+    slice::Chunks,
+    str::Chars,
     sync::mpsc::Sender,
 };
 
@@ -395,6 +402,22 @@ impl Application {
                 if total_length as u16 >= term_width {
                     height += 1
                 };
+                // let tag = data.get(0).unwrap();
+                // let mut val = data.get(1).unwrap().chars();
+                // let sub_string = (0..)
+                //     .map(|_| val.by_ref().take(term_width as usize).collect::<String>())
+                //     .take_while(|s| !s.is_empty())
+                //     .map(|s| Line::from(s))
+                //     .collect::<Vec<_>>();
+
+                // let lines = Text::from(sub_string);
+
+                // Row::new(vec![
+                //     Cell::from(Text::from(tag.to_owned())),
+                //     Cell::from(lines),
+                // ])
+                // .height(height)
+
                 Row::new(data.clone()).height(height)
             })
             .collect::<Vec<Row>>()
